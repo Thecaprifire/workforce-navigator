53d168f8e4dc2123f76369340875a17f65e43b1d
Ian Daluz <janiandaluz@yahoo.com>
2024-07-06 01:27:08 -0600

    edited codes in querry.sql and test.sql file

--- db/test.sql
+++ db/test.sql
@@ -1,3 +1,8 @@
-select * from department; 
-select * from role; 
-select * from employee;
+-- Show all rows from departments table
+SELECT * FROM departments;
+
+-- Show all rows from roles table
+SELECT * FROM roles;
+
+-- Show all rows from employee table
+SELECT * FROM employee;
